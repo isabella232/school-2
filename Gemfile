2a9cfc424f17d824ec1452dd8949ad085639eca2
Ivan Fefilov <ivfefilov@gmail.com>
2015-12-10 13:54:48 +0400

    add refinery settings

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'unicorn'
 gem 'slim-rails'
 
 gem 'refinerycms', '~> 3.0'
+gem 'refinerycms-settings', '~> 3.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210091954) do
+ActiveRecord::Schema.define(version: 20151210095418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,6 +125,21 @@ ActiveRecord::Schema.define(version: 20151210091954) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "refinery_settings", force: :cascade do |t|
+    t.string   "name"
+    t.text     "value"
+    t.boolean  "destroyable",     default: true
+    t.string   "scoping"
+    t.boolean  "restricted",      default: false
+    t.string   "form_value_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "slug"
+    t.string   "title"
+  end
+
+  add_index "refinery_settings", ["name"], name: "index_refinery_settings_on_name", using: :btree
 
   create_table "seo_meta", force: :cascade do |t|
     t.integer  "seo_meta_id"
