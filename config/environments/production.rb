b2ea1d40793c5e61cab65147cb21cd0e496eca5d
Ivan Fefilov <ivfefilov@gmail.com>
2016-08-17 16:30:32 +0400

    add mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,18 @@ Rails.application.configure do
   if config.respond_to?(:action_mailer)
     # config.action_mailer.raise_delivery_errors = false
   end
-
+  
+  #Action mailer smpt settings
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password:  ENV['SENDGRID_PASSWORD'],
+    domain: 'glazov-sport1.ru',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
