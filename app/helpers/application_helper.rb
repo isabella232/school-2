e74b3a77dd3cc440f834df99b6f2506bb19cd159
Ivan Fefilov <ivfefilov@gmail.com>
2016-12-06 12:17:47 +0400

    fix last posts block

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def root_page
     Refinery::Page.find_by(link_url: '/')
   end
+  
+  def last_blog_posts(count)
+    Refinery::Blog::Post.order('created_at DESC').take(count)
+  end  
 end
