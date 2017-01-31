170ee533162bd55e9d81601725ae14c0fbbe390a
Ivan Fefilov <ivfefilov@gmail.com>
2017-01-31 15:14:42 +0400

    fix gallery cover_image error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,5 +5,9 @@ module ApplicationHelper
   
   def last_blog_posts(count)
     Refinery::Blog::Post.order('created_at DESC').take(count)
-  end  
+  end
+  
+  def gallery_cover_image(gallery)
+    gallery.cover_image.present? ? gallery.cover_image.image.url : 'no_image_available.png'
+  end    
 end
